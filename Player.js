49f1e23e0cafa65d87ad034ea74e1c7687c9a049
GamerzKrish <74825009+GamerzKrish@users.noreply.github.com>
2021-04-08 19:11:09 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,39 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    //players folder-->player1
+    //players-->player2
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      //allPlayers contain separate info of players allPlayers= player1/player2
+      //allPlayers is variable , but multiple values(info of all the players in different memory location) it becomes array
+      allPlayers = data.val();
+    })
+  }
+}
